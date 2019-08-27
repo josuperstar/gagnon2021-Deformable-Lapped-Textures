8274f77d0c9ef6d4376ec8eb53d1ada66f8ff6b8
josuperstar <jonathangagnon@gmail.com>
2019-08-26 21:02:59 -0400

    Removing unused inheritance.

--- HoudiniPlugin/yu2011/Strategies/StrategyPatchSurfaceSynthesis.h
+++ HoudiniPlugin/yu2011/Strategies/StrategyPatchSurfaceSynthesis.h
@@ -1,14 +1,6 @@
 #ifndef __STRATEGYPATCHSURFACE_h__
 #define __STRATEGYPATCHSURFACE_h__
 
-#include <Math/Vec3.h>
-
-//#include <GaussianPyramid.h>
-
-#include <SOP/SOP_Node.h>
-#include <GEO/GEO_PrimPart.h>
-
-
 #include <Core/Deformations/ParametersDeformablePatches.h>
 
 using namespace std;
@@ -19,13 +11,6 @@ namespace Mokko {
 
 class StrategyPatchSurfaceSynthesis
 {
-public:
-    virtual ~StrategyPatchSurfaceSynthesis() {}
-    virtual bool SynthesisSurface( GU_Detail *gdp, ParametersDeformablePatches params) = 0;
-
-protected :
-
-
 
 };
 
